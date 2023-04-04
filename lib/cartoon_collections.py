a72896efef49f56b17bd6ec713ec075b270f6c8a
Yael Harosh <yaelharosh01@gmail.com>
2023-04-04 15:17:55 -0400

    pass tests

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,23 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(arr):
+    for index, name in enumerate(arr):
+        print(f'{index + 1}. {name}')
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(arr):
+    return [f'{name.capitalize()}!' for name in arr]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(arr):
+    for value in arr:
+        print(len(value))
+        if len(value) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(arr):
+    if "cheddar" in arr:
+        return "cheddar"
+    elif "gouda" in arr:
+        return "gouda"
+    elif "camembert" in arr:
+        return "camembert"
+    else:
+        return None
